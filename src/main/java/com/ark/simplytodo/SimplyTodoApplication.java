6a0bd3824fabbe348f89eb3bb7d4774b8d0c3f87
anirudhrk01 <anirudhrk01@gmail.com>
2024-11-12 10:12:43 +0530

    [base]

---
+++ src/main/java/com/ark/simplytodo/SimplyTodoApplication.java
@@ -0,0 +1,13 @@
+package com.ark.simplytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplyTodoApplication.class, args);
+    }
+
+}
