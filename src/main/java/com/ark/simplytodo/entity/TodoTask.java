0bf84491e3e0f0ab7a40c11379f97fad425e1c5b
anirudhrk01 <anirudhrk01@gmail.com>
2024-11-15 09:20:11 +0530

    phone

--- src/main/java/com/ark/simplytodo/entity/TodoTask.java
+++ src/main/java/com/ark/simplytodo/entity/TodoTask.java
@@ -1,15 +1,14 @@
 package com.ark.simplytodo.entity;
 
+import com.ark.simplytodo.annotation.Phone;
 import com.ark.simplytodo.enums.TodoTaskStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+
 
 
 import java.util.Date;
@@ -35,6 +34,9 @@ public class TodoTask {
     private Date createdAt= Date.from(java.time.Instant.now());
 
     private Set<String> tags;
+
+    @Phone
+    private String phone;
 
 
 }
